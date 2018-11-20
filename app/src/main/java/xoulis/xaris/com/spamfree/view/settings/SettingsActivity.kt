df5a0d29c2aa18710e879bd1dbae9b1b3b88704e
Haris Houlis <Charis1331@users.noreply.github.com>
2018-11-20 10:53:35 +0200

    Fixed google signIn bug, CodesFragment fixes
    ~Fixed a bug where user couldn't sign in with a google account
    ~Fixed and optimized CodesFragment
    ~Updated libraries

--- app/src/main/java/xoulis/xaris/com/spamfree/view/settings/SettingsActivity.kt
+++ app/src/main/java/xoulis/xaris/com/spamfree/view/settings/SettingsActivity.kt
@@ -7,12 +7,15 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.app.NavUtils
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.MenuItem
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.storage.FirebaseStorage
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
+import kotlinx.android.synthetic.main.activity_settings.*
 import xoulis.xaris.com.spamfree.*
 import xoulis.xaris.com.spamfree.data.vo.User
 import xoulis.xaris.com.spamfree.databinding.ActivitySettingsBinding
@@ -22,13 +25,17 @@ class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
     private val chatPhotoStorageRef =
-        FirebaseStorage.getInstance().reference.child("profile_photos")
+            FirebaseStorage.getInstance().reference.child("profile_photos")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_settings)
         binding.isLoading = true
         binding.handler = this
+
+        settings_back_arrow_imageView.setOnClickListener {
+            NavUtils.navigateUpFromSameTask(this)
+        }
 
         val viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         viewModel.userLiveData.observe(this, Observer {
@@ -44,10 +51,10 @@ class SettingsActivity : AppCompatActivity() {
             binding.settingsActivityRoot.showSnackBar(R.string.no_internet_connection)
         } else {
             CropImage.activity()
-                .setAspectRatio(1, 1)
-                .setMaxCropResultSize(5000, 5000)
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .start(this)
+                    .setAspectRatio(1, 1)
+                    .setMaxCropResultSize(5000, 5000)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this)
         }
     }
 
@@ -65,13 +72,13 @@ class SettingsActivity : AppCompatActivity() {
             selectedImageUri.lastPathSegment?.let {
                 val photoRef = chatPhotoStorageRef.child(it)
                 photoRef.putFile(selectedImageUri)
-                    .addOnSuccessListener { _ ->
-                        photoRef.downloadUrl
-                            .addOnCompleteListener { task ->
-                                check(task.isSuccessful) { "Failed to retrieve url for user photo" }
-                                updateUserPhotoPath(task.result.toString())
-                            }
-                    }
+                        .addOnSuccessListener { _ ->
+                            photoRef.downloadUrl
+                                    .addOnCompleteListener { task ->
+                                        check(task.isSuccessful) { "Failed to retrieve url for user photo" }
+                                        updateUserPhotoPath(task.result.toString())
+                                    }
+                        }
             }
         }
     }
@@ -86,9 +93,9 @@ class SettingsActivity : AppCompatActivity() {
         // Call server function
         val data = hashMapOf("imageUrl" to imageUrl)
         FirebaseFunctions
-            .getInstance("europe-west1")
-            .getHttpsCallable("updateUserImageInChats")
-            .call(data)
+                .getInstance("europe-west1")
+                .getHttpsCallable("updateUserImageInChats")
+                .call(data)
     }
 
     fun onStatusChangeClick(user: User) {
